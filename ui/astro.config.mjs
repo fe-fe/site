ffd84187dcd5ef23f1ce54be3b114f93db49aad2
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2026-09-06 19:09:30 -0300

    remake ui (#12)
    * feat: add lace border, custom font
    
    also removes placeholder icons
    
    * fix: set vite polling to fix hot reload on windows
    
    * feat: add basic navbar component
    
    * feat: add basic "back" button component
    
    * feat: add accent colors, utility styles
    
    * feat: update box component to the new design
    
    * feat: update layout and index to the new design
    
    * fix: remove unused imports from index

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -4,4 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maria.qa',
+    vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
+    },
+  },
 });
