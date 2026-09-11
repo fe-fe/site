02ba7f2dbeee42384f11cb4b42d464b0e14fdb10
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2026-09-11 19:39:59 -0300

    fix: adjust responsivity on recently played for small screens, box padding (#17)
    * fix: adjust recently played comp on small screens
    
    * fix: lower the amount of padding on outerbox

--- ui/src/components/Track.tsx
+++ ui/src/components/Track.tsx
@@ -20,7 +20,7 @@ interface TrackProps {
 export default function Track({ trackInfo }: TrackProps) {
     return (
         <>
-            <a href={trackInfo.url} className="scrobble w-auto sm:w-sm" target="_blank">
+            <a href={trackInfo.url} className="scrobble w-full sm:w-sm" target="_blank">
                 <div className="flex justify-between whitespace-nowrap">
                     <div className="inria-sans-bold overflow-hidden text-ellipsis min-w-0 flex-1">{trackInfo.artist["#text"]}</div>
                     <div className="overflow-hidden text-ellipsis min-w-0 flex-1 text-end">{trackInfo.name}</div>
